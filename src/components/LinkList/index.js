b110dbc93f819a0887a1c38a0043d335cb03a6ee
user <pengfluf@tutanota.com>
2018-03-25 12:06:37 +0300

    Subscriptions with WebSockets

--- src/components/LinkList/index.js
+++ src/components/LinkList/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import LinkCustom from '../LinkCustom';
 
@@ -13,6 +14,11 @@ class LinkList extends React.Component {
     this._updateCacheAfterVote = this._updateCacheAfterVote.bind(this);
   }
 
+  componentDidMount() {
+    this._subscribeToNewLinks();
+    this._subscribeToNewVotes();
+  }
+
   _updateCacheAfterVote(store, createVote, linkId) {
     const data = store.readQuery({ query: FEED_QUERY });
 
@@ -21,6 +27,76 @@ class LinkList extends React.Component {
     votedLink.votes = createVote.link.votes;
 
     store.writeQuery({ query: FEED_QUERY, data });
+  }
+
+  _subscribeToNewLinks() {
+    this.props.feedQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          newLink {
+            node {
+              id
+              url
+              description
+              createdAt
+              postedBy {
+                id
+                name
+              }
+              votes {
+                id
+                user {
+                  id
+                }
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newAllLinks = [subscriptionData.data.newLink.node, ...previous.feed.links];
+        const result = {
+          ...previous,
+          feed: {
+            links: newAllLinks,
+          },
+        };
+        return result;
+      },
+    });
+  }
+
+  _subscribeToNewVotes() {
+    this.props.feedQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          newVote {
+            node {
+              id
+              link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                  id
+                  name
+                }
+                votes {
+                  id
+                  user {
+                    id
+                  }
+                }
+              }
+              user {
+                id
+              }
+            }
+          }
+        }
+      `,
+    });
   }
 
   render() {
@@ -64,6 +140,7 @@ LinkList.propTypes = {
     feed: PropTypes.shape({
       links: PropTypes.array,
     }),
+    subscribeToMore: PropTypes.func,
   }),
 };
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
-import { ApolloLink } from 'apollo-client-preset';
+import { ApolloLink, split } from 'apollo-client-preset';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
@@ -19,6 +21,16 @@ import { AUTH_TOKEN } from './components/Login/constants';
 import registerServiceWorker from './registerServiceWorker';
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+});
 
 const middlewareAuthLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem(AUTH_TOKEN);
@@ -36,8 +48,19 @@ const httpLinkWithAuthToken =
   middlewareAuthLink
     .concat(httpLink);
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition'
+            &&
+            operation === 'subscription';
+  },
+  wsLink,
+  httpLinkWithAuthToken
+);
+
 const client = new ApolloClient({
-  link: httpLinkWithAuthToken,
+  link,
   cache: new InMemoryCache(),
 });
 
